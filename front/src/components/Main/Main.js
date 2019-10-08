4509834297b4f87a6af660ff475be0282c877ec3
HyeonJun <41042989+znakwkd30@users.noreply.github.com>
2019-10-09 01:38:16 +0900

    Update Main.js

--- front/src/components/Main/Main.js
+++ front/src/components/Main/Main.js
@@ -1,27 +1,123 @@
-import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/Styles';
+import React, { useEffect, useCallback, useState, Fragment } from 'react';
 import Nav from '../Nav';
-
-
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import Axios from '../../Axios/Axios';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert'
+const useStyles = makeStyles(theme => ({
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+}));
 
 function Main() {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+  
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+
+    const [product, setProduct] = useState({});
+    async function mainProduct() {
+        let result = await Axios({
+            url: 'api/product/main',
+            method: 'get'
+        })
+        console.log(result);
+        setProduct(result.data.productList);
+        console.log(product);
+    }
+
+    useEffect(() => {
+        mainProduct()
+    }, []);
+
     return (
-        <Nav/>
+        <Fragment>
+            <Nav/>
+            <Card className={classes.card}>
+            <CardHeader
+                avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                    R
+                </Avatar>
+                }
+                action={
+                <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                </IconButton>
+                }
+                title="Shrimp and Chorizo Paella"
+                subheader="September 14, 2016"
+            />
+            <CardMedia
+                className={classes.media}
+                image="/static/images/cards/paella.jpg"
+                title="Paella dish"
+            />
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {product.name}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+                </IconButton>
+                <IconButton
+                className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+                >
+                <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                <Typography paragraph>Method:</Typography>
+                <Typography paragraph>
+                    Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                    minutes.
+                </Typography>
+                </CardContent>
+            </Collapse>
+            </Card>
+        </Fragment>
     );
 }
 
