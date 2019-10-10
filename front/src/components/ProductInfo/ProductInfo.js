f5a8daa9b619c2c4e516f664c554941a4f446407
ShinMS <39144276+QMCHE@users.noreply.github.com>
2019-10-10 17:44:05 +0900

    Create ProductInfo.js

---
+++ front/src/components/ProductInfo/ProductInfo.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import Nav from '../Nav';
+
+function ProductInfo(){
+    return(
+        <Fragment>
+            <Nav/>
+        </Fragment>
+    )
+}
+
+export default ProductInfo;
