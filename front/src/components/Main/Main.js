2c5d3726b5712b1ed04bf497a81a97096b5a5387
ShinMS <39144276+QMCHE@users.noreply.github.com>
2019-10-10 17:55:49 +0900

    Update Main.js

--- front/src/components/Main/Main.js
+++ front/src/components/Main/Main.js
@@ -22,45 +22,50 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(theme => ({
+    root: {
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+    },
     card: {
-      maxWidth: 400,
+        margin: "20px",
+        maxWidth: 400,
     },
     media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
+        height: 0,
+        paddingTop: '56.25%', // 16:9
     },
     expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
     },
     expandOpen: {
-      transform: 'rotate(180deg)',
+        transform: 'rotate(180deg)',
     },
     avatar: {
-      backgroundColor: red[500],
+        backgroundColor: red[500],
     },
 }));
 
 function Main() {
     const classes = useStyles();
     const [expanded, setExpanded] = React.useState(false);
-  
+
     const handleExpandClick = () => {
-      setExpanded(!expanded);
+        setExpanded(!expanded);
     };
 
-    const [products, setProducts] = useState({});
+    const [products, setProducts] = useState([]);
     const [images, setImages] = useState({});
     async function mainProduct() {
         let result = await Axios({
             url: 'api/product/main',
             method: 'get'
         })
-        console.log(result);
-        console.log(result.data.productList[0].Images[0].src)
+        console.log(result.data.productList);
         setProducts(result.data.productList);
         setImages(result.data.productList.Images);
     }
@@ -71,58 +76,53 @@ function Main() {
 
     return (
         <Fragment>
-            <Nav/>
-            <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
-            {Object.keys(products).map((product, i) => (
-                <div style={{margin: "20px", width: "420px"}} key={ i }>
-                <Card className={classes.card}>
-                <CardHeader
-                    action={
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                    </IconButton>
-                    }
-                    title={products[i].productName}
-                    subheader={<Time value={products[i].updateDay} format="YYYY/MM/DD hh:mm"/>}
-                />
-                <img src={"http://10.80.163.141:3065/"+products[i].Images[0].src} style={{width: 350, height: 200}}></img>
-                {/* <CardMedia
-                    className={classes.media}
-                    image={products[i].Images[0].src}
-                    title="Paella dish"
-                /> */}
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p"
-                                style={{fontSize: "24px", fontFamily: "궁서체"}}>
-                        {products[i].price}원
-                    </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                    </IconButton>
-                    <IconButton
-                    className={clsx(classes.expand, {
-                        [classes.expandOpen]: expanded,
-                    })}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                    >
-                    <ExpandMoreIcon />
-                    </IconButton>
-                </CardActions>
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                    <Typography paragraph>상세설명 : {products[i].description}</Typography>
-                    <Typography paragraph>해시태그 : <Link to="/search">{products[i].hashtag}</Link></Typography>
-                    <Typography paragraph>카테고리 : <Link to="/search">{products[i].category}</Link></Typography>
-                    </CardContent>
-                </Collapse>
-                </Card>
-                </div>
-                ))}
-            </div>            
+            <Nav />
+            <div className={classes.root}>
+                {products.map((item, key) => {
+                    return (
+                        <Card className={classes.card}>
+                            <CardHeader
+                                action={
+                                    <IconButton aria-label="settings">
+                                        <MoreVertIcon />
+                                    </IconButton>
+                                }
+                                title={item.productName}
+                                subheader={<Time value={item.updateDay} format="YYYY/MM/DD hh:mm" />}
+                            />
+                            <img src={"http://10.80.163.141:3065/" + item.Images[0].src} style={{ width: 350, height: 200 }}></img>
+                            <CardContent>
+                                <Typography variant="body2" color="textSecondary" component="p"
+                                    style={{ fontSize: "24px", fontFamily: "궁서체" }}>
+                                    {item.price}원
+                                    </Typography>
+                            </CardContent>
+                            <CardActions disableSpacing>
+                                <IconButton aria-label="add to favorites">
+                                    <FavoriteIcon />
+                                </IconButton>
+                                <IconButton
+                                    className={clsx(classes.expand, {
+                                        [classes.expandOpen]: expanded,
+                                    })}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </IconButton>
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <Typography paragraph>상세설명 : {item.description}</Typography>
+                                    <Typography paragraph>해시태그 : <Link to={"/search/" + item.hashtag}>{item.hashtag}</Link></Typography>
+                                    <Typography paragraph>카테고리 : <Link to={"/search/" + item.category}>{item.category}</Link></Typography>
+                                </CardContent>
+                            </Collapse>
+                        </Card>
+                    )
+                })}
+            </div>
         </Fragment>
     );
 }
