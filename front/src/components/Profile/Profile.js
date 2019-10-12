f428546848b28093199b097df4ef7d5c375bd0b9
ShinMS <39144276+QMCHE@users.noreply.github.com>
2019-10-12 10:29:17 +0900

    Update Profile.js

--- front/src/components/Profile/Profile.js
+++ front/src/components/Profile/Profile.js
@@ -5,8 +5,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 
 import Nav from '../Nav';
@@ -62,12 +60,9 @@ function Profile() {
                         <Typography variant="h4" align="center">
                             {userInfo.name}
                         </Typography>
-                        <Grid item>
-                            <ButtonGroup size="small" aria-label="small outlined button group">
-                                <Button>정보 수정</Button>
-                                <Button>Two</Button>
-                            </ButtonGroup>
-                        </Grid>
+                        <Button variant="contained" className={classes.button}>
+                            Default
+                        </Button>
                     </div>
                 </Card>
             </Fragment>
