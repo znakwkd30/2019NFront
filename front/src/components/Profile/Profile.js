4b247373f4f61dd4a3eaca876d0dad86fe375224
HyeonJun <41042989+znakwkd30@users.noreply.github.com>
2019-10-14 19:25:17 +0900

    [Update] Profile

--- front/src/components/Profile/Profile.js
+++ front/src/components/Profile/Profile.js
@@ -1,21 +1,39 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Axios from '../../Axios/Axios';
 import { makeStyles } from '@material-ui/core/styles';
-
+import Time from 'react-time-format';
+import { Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-
 import Nav from '../Nav';
-
 import defaultImg from '../../Assets/noImg.png';
-import { CardContent } from '@material-ui/core';
+import clsx from 'clsx';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+    },
     card: {
         margin: "20px auto",
         width: "60%",
+    },
+    itemCard: {
+        margin: "20px",
+        width: 300,
+        height: 420,
     },
     main: {
         margin: "60px auto 20px",
@@ -26,10 +44,24 @@ const useStyles = makeStyles({
         width: 130,
         height: 130,
     },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
     avatar:{
         margin: "0 auto",
         width: "50px",
         height: "50px",
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
     },
     Typography: {
         margin: "20px 0 0 0",
@@ -42,14 +74,31 @@ const useStyles = makeStyles({
 
     },
 
-});
+}));
 
 function Profile() {
     const classes = useStyles();
 
     const [log] = React.useState(window.localStorage.getItem("token") === null && window.sessionStorage.getItem("token") === null);
     const [userInfo, setUserInfo] = React.useState([]);
+    const [expanded, setExpanded] = React.useState(false);
     const [userImg, setUserImg] = React.useState();
+    const [products, setProducts] = useState([]);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    async function searchProduct() {
+        let result;
+        result = await Axios({
+            url: 'api/product/myProduct/',
+            headers: {"token" : window.localStorage.getItem("token") || window.sessionStorage.getItem("token")},
+            method: 'get'
+        })
+        
+        setProducts(result.data.productList);            
+    }
 
     async function getProfile() {
         let result = await Axios({
@@ -64,6 +113,7 @@ function Profile() {
 
     React.useEffect(() => {
         getProfile();
+        searchProduct();
     }, []);
 
     if (log) {
@@ -80,24 +130,57 @@ function Profile() {
                             {userInfo.name}
                         </Typography>
                         <CardContent className={classes.cardcontent}>
-                            
                         </CardContent>
                     </div>
                 </Card>
-                <Card>
-                    <div className={classes.myproduct}>
-                            
-                    </div>
-                    <div className={classes.heartProduct}>
-                        <Avatar alt="profileImg" src={userInfo.length === 0 ? defaultImg : "http://10.80.163.141:3065/\\" + userImg} className={classes.avatar} />
-                        <Typography variant="h4" align="center">
-                            {userInfo.name}
-                        </Typography>
-                        <Button variant="contained" className={classes.button}>
-                            Default
-                        </Button>
-                    </div>
-                </Card>
+                <Typography variant="h4" align="center">내 상품</Typography>
+                <div className={classes.root} style={{ width: "80%", margin: "auto" }}> 
+                {products.map((item, key) => {
+                    return (
+                        <Card className={classes.itemCard} key={key}>
+                            <CardHeader
+                                action={
+                                    <IconButton aria-label="settings">
+                                        <MoreVertIcon />
+                                    </IconButton>
+                                }
+                                title={item.productName}
+                                subheader={<Time value={item.updateDay} format="YYYY/MM/DD hh:mm" />}
+                            />
+                            <img src={item.Images.length === 0 ? defaultImg : "http://10.80.163.141:3065/" + item.Images[0].src} style={{ width: 350, height: 200 }} alt={item.productName}></img>
+                            {/* <img src={"http://10.80.163.141:3065/" + item.Images[0].src} style={{ width: 350, height: 200 }}></img> */}
+                            <CardContent>
+                                <Typography variant="body2" color="textSecondary" component="p"
+                                    style={{ fontSize: "24px", fontFamily: "궁서체" }}>
+                                    {item.price}원
+                                    </Typography>
+                            </CardContent>
+                            <CardActions disableSpacing>
+                                <IconButton aria-label="add to favorites">
+                                    <FavoriteIcon />
+                                </IconButton>
+                                <IconButton
+                                    className={clsx(classes.expand, {
+                                        [classes.expandOpen]: expanded,
+                                    })}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </IconButton>
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <Typography paragraph>상세설명 : {item.description}</Typography>
+                                    <Typography paragraph>해시태그 : <Link to={"/search/" + item.hashtag}>{item.hashtag}</Link></Typography>
+                                    <Typography paragraph>카테고리 : <Link to={"/search/" + item.category}>{item.category}</Link></Typography>
+                                </CardContent>
+                            </Collapse>
+                        </Card>
+                    )
+                })}
+            </div>
             </Fragment>
         )
     }
